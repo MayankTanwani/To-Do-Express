84632ff106bd8c2002d42cdf295810644197d6fe
Mayank Tanwani <tanwani.mayank@gmail.com>
2018-06-19 13:47:45 +0530

    port changed

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function postFile(req,res) {
         console.log('Complete')
     })
 }
-app.set('port',process.env.Port || 4000)
+app.set('port',process.env.PORT || 4000)
 app.listen(app.get('port'),function () {
     console.log('Server started on http://localhost:2323/')
 })
